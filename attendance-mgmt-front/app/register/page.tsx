472b876d7d14a43166291006ee8ea30b0913df90
chaolinmonk <dquiozagonzalez@gmail.com>
2026-04-02 21:56:07 -0300

    fix: cambios register page

--- attendance-mgmt-front/app/register/page.tsx
+++ attendance-mgmt-front/app/register/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import RegisterForm from "@/components/forms/RegisterForm";
 export default function Home() {
+  
+  
   return (
     <div className="grid grid-cols-6 grid-rows-6 h-screen gap-2">
       <RegisterForm/>
